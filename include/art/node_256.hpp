c1858b9535e59c548df47427db88aa735e109dc8
chuqingG <gao688@purdue.edu>
2024-01-08 20:38:11 +0000

    update statistics

--- include/art/node_256.hpp
+++ include/art/node_256.hpp
@@ -12,131 +12,149 @@
 
 namespace art {
 
-template <class T> class node_48;
+template <class T>
+class node_48;
 
-template <class T> class node_256 : public inner_node<T> {
-  friend class node_48<T>;
+template <class T>
+class node_256 : public inner_node<T> {
+    friend class node_48<T>;
 
 public:
-  node_256();
+    node_256();
 
-  node<T> **find_child(char partial_key) override;
-  void set_child(char partial_key, node<T> *child) override;
-  node<T> *del_child(char partial_key) override;
-  inner_node<T> *grow() override;
-  inner_node<T> *shrink() override;
-  bool is_full() const override;
-  bool is_underfull() const override;
+    node<T> **find_child(char partial_key) override;
+    void set_child(char partial_key, node<T> *child) override;
+    node<T> *del_child(char partial_key) override;
+    inner_node<T> *grow() override;
+    inner_node<T> *shrink() override;
+    bool is_full() const override;
+    bool is_underfull() const override;
 
-  char next_partial_key(char partial_key) const override;
+    char next_partial_key(char partial_key) const override;
 
-  char prev_partial_key(char partial_key) const override;
+    char prev_partial_key(char partial_key) const override;
 
-  int n_children() const override;
+    int n_children() const override;
 
-  void get_size(int &numNodes, int &numNonleaf, int &totalBranching,
-                int &usedBranching, unsigned long &totalKeySize) override;
-  int get_height() override;
+    void get_size(int &numNodes, int &numNonleaf, int &totalBranching,
+                  int &usedBranching, unsigned long &totalKeySize) override;
+    int get_height() override;
 
 private:
-  uint16_t n_children_ = 0;
-  std::array<node<T> *, 256> children_;
+    uint16_t n_children_ = 0;
+    std::array<node<T> *, 256> children_;
 };
 
-template <class T> node_256<T>::node_256() { children_.fill(nullptr); }
+template <class T>
+node_256<T>::node_256() {
+    children_.fill(nullptr);
+}
 
-template <class T> node<T> **node_256<T>::find_child(char partial_key) {
-  return children_[128 + partial_key] != nullptr ? &children_[128 + partial_key]
-                                                 : nullptr;
+template <class T>
+node<T> **node_256<T>::find_child(char partial_key) {
+    return children_[128 + partial_key] != nullptr ? &children_[128 + partial_key] : nullptr;
 }
 
 template <class T>
 void node_256<T>::set_child(char partial_key, node<T> *child) {
-  children_[128 + partial_key] = child;
-  ++n_children_;
+    children_[128 + partial_key] = child;
+    ++n_children_;
 }
 
-template <class T> node<T> *node_256<T>::del_child(char partial_key) {
-  node<T> *child_to_delete = children_[128 + partial_key];
-  if (child_to_delete != nullptr) {
-    children_[128 + partial_key] = nullptr;
-    --n_children_;
-  }
-  return child_to_delete;
-}
-
-template <class T> inner_node<T> *node_256<T>::grow() {
-  throw std::runtime_error("node_256 cannot grow");
-}
-
-template <class T> inner_node<T> *node_256<T>::shrink() {
-  auto new_node = new node_48<T>();
-  new_node->prefix_ = this->prefix_;
-  new_node->prefix_len_ = this->prefix_len_;
-  for (int partial_key = 0; partial_key < 256; ++partial_key) {
-    if (children_[128 + partial_key] != nullptr) {
-      new_node->set_child(partial_key, children_[128 + partial_key]);
+template <class T>
+node<T> *node_256<T>::del_child(char partial_key) {
+    node<T> *child_to_delete = children_[128 + partial_key];
+    if (child_to_delete != nullptr) {
+        children_[128 + partial_key] = nullptr;
+        --n_children_;
     }
-  }
-  delete this;
-  return new_node;
+    return child_to_delete;
 }
 
-template <class T> bool node_256<T>::is_full() const {
-  return n_children_ == 256;
+template <class T>
+inner_node<T> *node_256<T>::grow() {
+    throw std::runtime_error("node_256 cannot grow");
 }
 
-template <class T> bool node_256<T>::is_underfull() const {
-  return n_children_ == 48;
-}
-
-template <class T> char node_256<T>::next_partial_key(char partial_key) const {
-  while (true) {
-    if (children_[128 + partial_key] != nullptr) {
-      return partial_key;
+template <class T>
+inner_node<T> *node_256<T>::shrink() {
+    auto new_node = new node_48<T>();
+    new_node->prefix_ = this->prefix_;
+    new_node->prefix_len_ = this->prefix_len_;
+    for (int partial_key = 0; partial_key < 256; ++partial_key) {
+        if (children_[128 + partial_key] != nullptr) {
+            new_node->set_child(partial_key, children_[128 + partial_key]);
+        }
     }
-    if (partial_key == 127) {
-      throw std::out_of_range("provided partial key does not have a successor");
-    }
-    ++partial_key;
-  }
+    delete this;
+    return new_node;
 }
 
-template <class T> char node_256<T>::prev_partial_key(char partial_key) const {
-  while (true) {
-    if (children_[128 + partial_key] != nullptr) {
-      return partial_key;
-    }
-    if (partial_key == -128) {
-      throw std::out_of_range(
-          "provided partial key does not have a predecessor");
-    }
-    --partial_key;
-  }
+template <class T>
+bool node_256<T>::is_full() const {
+    return n_children_ == 256;
 }
 
-template <class T> int node_256<T>::n_children() const { return n_children_; }
+template <class T>
+bool node_256<T>::is_underfull() const {
+    return n_children_ == 48;
+}
+
+template <class T>
+char node_256<T>::next_partial_key(char partial_key) const {
+    while (true) {
+        if (children_[128 + partial_key] != nullptr) {
+            return partial_key;
+        }
+        if (partial_key == 127) {
+            throw std::out_of_range("provided partial key does not have a successor");
+        }
+        ++partial_key;
+    }
+}
+
+template <class T>
+char node_256<T>::prev_partial_key(char partial_key) const {
+    while (true) {
+        if (children_[128 + partial_key] != nullptr) {
+            return partial_key;
+        }
+        if (partial_key == -128) {
+            throw std::out_of_range(
+                "provided partial key does not have a predecessor");
+        }
+        --partial_key;
+    }
+}
+
+template <class T>
+int node_256<T>::n_children() const {
+    return n_children_;
+}
 
 template <class T>
 void node_256<T>::get_size(int &numNodes, int &numNonleaf, int &totalBranching,
                            int &usedBranching, unsigned long &totalKeySize) {
-  numNodes++;
-  numNonleaf++;
-  totalBranching += 256;
-  usedBranching += this->n_children_;
-  totalKeySize += 256 + strlen(this->prefix_) + sizeof(this->prefix_len_);
-  for (int i = 0; i < n_children_; ++i) {
-    children_[i]->get_size(numNodes, numNonleaf, totalBranching, usedBranching,
-                           totalKeySize);
-  }
+    numNodes++;
+    numNonleaf++;
+    totalBranching += 256;
+    usedBranching += this->n_children_;
+    totalKeySize += 256 + strlen(this->prefix_) + sizeof(this->prefix_len_);
+    for (int i = 0; i < 256; ++i) {
+        if (children_[i])
+            children_[i]->get_size(numNodes, numNonleaf, totalBranching, usedBranching,
+                                   totalKeySize);
+    }
 }
 
-template <class T> int node_256<T>::get_height() {
-  int maxHeight = 0;
-  for (int i = 0; i < n_children_; ++i) {
-    maxHeight = max(maxHeight, children_[i]->get_height());
-  }
-  return maxHeight + 1;
+template <class T>
+int node_256<T>::get_height() {
+    int maxHeight = 0;
+    for (int i = 0; i < 256; ++i) {
+        if (children_[i])
+            maxHeight = max(maxHeight, children_[i]->get_height());
+    }
+    return maxHeight + 1;
 }
 
 } // namespace art
